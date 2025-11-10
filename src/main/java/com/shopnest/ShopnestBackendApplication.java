75988db3f7d5e805630be1c7e64789e99fbca0ad
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-10 15:16:37 +0530

    Initial commit

---
+++ src/main/java/com/shopnest/ShopnestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopnestBackendApplication.class, args);
+	}
+
+}
