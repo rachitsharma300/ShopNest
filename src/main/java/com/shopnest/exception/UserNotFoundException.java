0bf331177dcc9218295fabad8dc27418f07ace62
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-11 23:48:21 +0530

    Added UserNotFoundException for handling missing user scenarios

--- src/main/java/com/shopnest/exception/UserNotFoundException.java
+++ src/main/java/com/shopnest/exception/UserNotFoundException.java
@@ -1,4 +1,16 @@
 package com.shopnest.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Long userId) {
+        super("User not found with id: " + userId);
+    }
+
+    public UserNotFoundException(String email, String password) {
+        super("Invalid email or password");
+    }
 }
