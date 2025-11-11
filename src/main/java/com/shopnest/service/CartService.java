daf8498e0ca12f72be4d9a469ba9e3d85942ad83
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-11 23:32:20 +0530

    Implemented CartService interface

--- src/main/java/com/shopnest/service/CartService.java
+++ src/main/java/com/shopnest/service/CartService.java
@@ -1,4 +1,11 @@
 package com.shopnest.service;
 
-public class CartService {
+import com.shopnest.dto.CartDTO;
+
+public interface CartService {
+    CartDTO getCartByUserId(Long userId);
+    void addToCart(Long userId, Long productId, Integer quantity);
+    void updateCartItem(Long itemId, Integer quantity);
+    void removeFromCart(Long itemId);
+    void clearCart(Long userId);
 }
